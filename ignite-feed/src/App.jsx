fc1272707ebab92be639633a26736131045da011
Arthur Fabricyo <arthur.fabricyo@gmail.com>
2025-09-01 18:04:14 -0300

    Adicionando react no portifólo

---
+++ ignite-feed/src/App.jsx
@@ -0,0 +1,15 @@
+import { Header } from "./components/header"
+import "./global.css"
+
+function App() {
+
+
+  return (
+    <>
+      <Header />
+      <h1>Hello world</h1>
+    </>
+  )
+}
+
+export default App

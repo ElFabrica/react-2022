6543b4f533826764e0cfcfc9ee9830add3fc472e
Arthur Fabricyo <arthur.fabricyo@gmail.com>
2025-09-02 09:13:25 -0300

    sidebar inicializado

--- ignite-feed/src/App.jsx
+++ ignite-feed/src/App.jsx
@@ -1,14 +1,20 @@
-import { Header } from "./components/header"
+import { Header } from "./components/Header"
 import "./global.css"
-
+import styles from "./app.module.css"
+import { Sidebar } from "./components/Sidebar"
 function App() {
 
 
   return (
-    <>
+    <div>
       <Header />
-      <h1>Hello world</h1>
-    </>
+      <div className={styles.wrapper}>
+      <Sidebar/>
+      <main>
+        Meus posts posts Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste adipisci doloremque vitae cum beatae! Suscipit nobis vero dolores doloribus dicta incidunt voluptatum nihil labore natus hic. Perferendis temporibus sint blanditiis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore nemo iure voluptas pariatur quaerat laudantium sint nesciunt eligendi consequatur facilis. Aliquid inventore omnis minus est aut nobis iste consequuntur soluta. Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima sequi a iure, eos unde, magnam ipsum deleniti aperiam culpa veniam tempora repudiandae quam. Dolore impedit, quia animi reiciendis doloribus sit.
+      </main>
+      </div>
+    </div>
   )
 }
 
--- ignite-feed/src/components/header.jsx
+++ ignite-feed/src/components/header.jsx
@@ -1,9 +1,16 @@
-import "./header.css"
+import styles from "./header.module.css"
+import igniteLogo from "../assets/ignite-logo.svg"
+
+console.log(igniteLogo)
 
 export function Header() {
-    return (
-      <div className="header">
-        Olha o header ai
-      </div>
-    )
+  return (
+    <div className={styles.header}>
+      <img src={igniteLogo} alt="Logo do Ignite"/>
+      <strong>Iginte Feed</strong>
+      <main>
+        
+      </main>
+    </div>
+  )
 }

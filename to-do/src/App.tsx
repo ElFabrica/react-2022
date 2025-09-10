f90e456dab9a3b95606974c172c83ea49e96f6a2
Arthur Fabricyo <arthur.fabricyo@gmail.com>
2025-09-10 08:34:13 -0300

    componente de icon

--- to-do/src/App.tsx
+++ to-do/src/App.tsx
@@ -1,4 +1,11 @@
 import Text from "./components/Text";
+import Icon from "./components/Icon";
+import TrashIcon from "./assets/icons/trash.svg?react";
+import CheckIcon from "./assets/icons/check.svg?react"
+import PencilIcon from "./assets/icons/pencil.svg?react"
+import PlusIcon from "./assets/icons/plus.svg?react"
+import SpinnerIcon from "./assets/icons/spinner.svg?react"
+import XIcon from "./assets/icons/x.svg?react"
 
 export default function App() {
 
@@ -7,6 +14,13 @@ export default function App() {
       <Text variant="body-md" className="text-pink-base"> Olá mundo </Text>
       <Text variant="body-md-bold" className="text-green-base"> Olá mundo </Text>
       <Text variant="body-sm-bold"> Olá mundo </Text>
+      <Icon svg={TrashIcon} className="fill-pink-base"/>
+      <Icon svg={CheckIcon}/>
+      <Icon svg={PlusIcon}/>
+      <Icon svg={ SpinnerIcon}animate />
+      <Icon svg={PencilIcon} />
+      <Icon svg={XIcon} />
+      
     </div>
 
   )
---
+++ to-do/src/components/Icon.tsx
@@ -0,0 +1,22 @@
+import React from "react"
+import {cva, type VariantProps} from "class-variance-authority"
+
+export const icoVariants = cva("", {
+    variants: {
+        animate:{
+            false: "",
+            true: "animate-spin"
+        }
+    },
+    defaultVariants: {
+        animate: false
+    }
+}) 
+
+interface IconProps extends React.ComponentProps<"svg">, VariantProps<typeof icoVariants >{
+    svg: React.FC<React.ComponentProps<"svg">>    
+}
+
+export default function Icon({svg: SvgComponent,className, animate,...props}: IconProps){
+    return <SvgComponent className={icoVariants({animate, className})}{...props}/>
+}
